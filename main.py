45ad6cfe23e2e7e90d4fb81a309f45080f4ed344
teimoor1404-tele <teimoor.1404@gmail.com>
2026-09-25 22:29:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import os, telegram
-from telegram.ext import Updater, CommandHandler
+import os
+from flask import Flask
+from telegram import Bot
+import threading
 
+app = Flask(__name__)
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("MY_CHAT_ID")
+bot = Bot(token=TOKEN)
 
-updater = Updater(token=TOKEN, use_context=True)
-updater.dispatcher.add_handler(CommandHandler("start", lambda u,c: u.message.reply_text("ربات فعال است")))
-updater.start_polling()
-updater.idle()
+@app.route("/")
+def health():
+    return "OK", 200
+
+def start():
+    try:
+        bot.send_message(chat_id=CHAT_ID, text="ربات روی رندر فعال شد")
+    except:
+        pass
+
+if __name__ == "__main__":
+    threading.Thread(target=start, daemon=True).start()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
